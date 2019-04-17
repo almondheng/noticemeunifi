94219a240d2260f6eb99777c07bed3358c4db08a
BenLee1998 <46629864+BenLee1998@users.noreply.github.com>
2019-04-18 02:55:51 +0800

    fusionchart

--- dashboard/src/main.js
+++ dashboard/src/main.js
@@ -5,8 +5,13 @@ import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import 'vue2-timeago/dist/vue2-timeago.css'
+import VueFusionCharts from 'vue-fusioncharts';
+import FusionCharts from 'fusioncharts';
+import Charts from 'fusioncharts/fusioncharts.charts';
+import FusionTheme from 'fusioncharts/themes/fusioncharts.theme.fusion'
 
 Vue.config.productionTip = false
+Vue.use(VueFusionCharts, FusionCharts, Charts, FusionTheme)
 
 new Vue({
   router,
